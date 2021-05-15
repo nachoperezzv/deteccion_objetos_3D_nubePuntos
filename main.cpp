59433f2194e66ac2c9611bbbdb783ed37569e41c
nachoperezzv <80114719+nachoperezzv@users.noreply.github.com>
2021-05-15 11:40:36 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <boost/thread/thread.hpp>
+#include <pcl/common/common_headers.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/common/transforms.h>
+#include <boost/algorithm/string.hpp>
+#include <math.h>
+
+
+using namespace std;
+
+
+int main(int argc, char** argv){
+
+	//boost::thread visualizer;
+	//string pcdFile;
+
+
+	return 0;
+}
